e85ac79ad795e02f05ae5e25c0685b37f03b56cf
andreaszico <zicoaritonang@gmail.com>
2023-04-20 13:49:18 +0700

    add doku config

--- config/doku.php
+++ config/doku.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'BASE_URL' => [
-        'sandbox' => 'https://api-sandbox.doku.com',
-        'production' => 'https://api.doku.com',
+    "BASE_URL" => [
+        "sandbox" => "https://api-sandbox.doku.com",
+        "production" => "https://api.doku.com",
     ],
-    'PAYMENTS' => [
-        'CHECKOUT' => [
-            'payment.url' => '/checkout/v1/payment',
+    "PAYMENTS" => [
+        "CHECKOUT" => [
+            "payment.url" => "/checkout/v1/payment",
             "currency" => "IDR",
             "callback_url" => "http://doku.com/",
             "callback_url_cancel" => "https://doku.com",
@@ -21,6 +21,6 @@ return [
                 ]
             ],
         ],
-        'DIRECT_API' => [],
+        "DIRECT_API" => [],
     ],
 ];
