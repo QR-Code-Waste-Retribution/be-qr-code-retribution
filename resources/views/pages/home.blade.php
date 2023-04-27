3de5d7223b7c0cb58e453257210ab4adf0dff9ec
andreaszico <zicoaritonang@gmail.com>
2023-04-27 20:59:09 +0700

    fixing dashboard

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,63 +14,178 @@
     <!-- Left side columns -->
     <div class="col-lg-12">
         <div class="row">
+            <div class="col-12">
+                <p class="fw-bold">Informasi keuangan retribusi bulanan</p>
+                <div class="row">
 
-            <!-- Revenue Card -->
-            <div class="col-xxl-3 col-md-5">
-                <a href="{{ route('dashboard.income') }}">
-                    <div class="card info-card revenue-card" data-bs-toggle="tooltip" data-bs-placement="right"
-                        data-bs-custom-class="custom-tooltip" data-bs-title="Klik untuk melihat lebih detail pemasukan">
-                        <div class="card-body">
-                            <h5 class="card-title">Pemasukan <span>| Bulan Ini</span></h5>
+                    <!-- Revenue Card -->
+                    <div class="col-xxl-3 col-md-5">
+                        <a href="{{ route('dashboard.income') }}">
+                            <div class="card info-card revenue-card" data-bs-toggle="tooltip" data-bs-placement="right"
+                                data-bs-custom-class="custom-tooltip"
+                                data-bs-title="Klik untuk melihat lebih detail pemasukan">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pemasukan <span>| Bulan Ini</span></h5>
 
-                            <div class="d-flex align-items-center">
-                                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                    <i class="bi bi-currency-dollar"></i>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-currency-dollar"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6 class="fs-4">Rp.
+                                                {{ number_format($income['cash'] + $income['noncash'], 2) }} -,</h6>
+                                            <span class="text-success small pt-1 fw-bold"></span><span
+                                                class="text-muted small pt-2 ps-1">21 Februari 2023</span>
+
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="ps-3">
-                                    <h6 class="fs-4">Rp. {{ number_format($income['cash'] + $income['noncash'], 2) }} -,</h6>
-                                    <span class="text-success small pt-1 fw-bold"></span><span
-                                        class="text-muted small pt-2 ps-1">21 Februari 2023</span>
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </a>
-            </div><!-- End Revenue Card -->
-
-            <!-- Sales Card -->
-            <div class="col-xxl-4 col-md-3">
-                <h5 class="card-title text-center fs-6">$ Tunai</h5>
-                <div class="card info-card sales-card pt-4 card-dashboard">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="p-0">
-                                <h6 class="fs-5">Rp. {{ number_format($income['cash'], 2) }} -,</h6>
-                                <span class="text-success small pt-1 fw-bold"></span> <span
-                                    class="text-muted small pt-2 ps-1">21 Februari 2023</span>
 
                             </div>
-                        </div>
-                    </div>
+                        </a>
+                    </div><!-- End Revenue Card -->
+
+                    <!-- Revenue Card -->
+                    <div class="col-xxl-3 col-md-5">
+                        <a href="{{ route('dashboard.income') }}">
+                            <div class="card info-card revenue-card" data-bs-toggle="tooltip" data-bs-placement="right"
+                                data-bs-custom-class="custom-tooltip"
+                                data-bs-title="Klik untuk melihat lebih detail pemasukan">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total yang harus tercapai di Bulan Ini</h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-currency-dollar"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6 class="fs-4">Rp.
+                                                {{ number_format($income['cash'] + $income['noncash'], 2) }} -,</h6>
+                                            <span class="text-success small pt-1 fw-bold"></span><span
+                                                class="text-muted small pt-2 ps-1">21 Februari 2023</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div><!-- End Revenue Card -->
 
                 </div>
-            </div><!-- End Sales Card -->
-            <div class="col-xxl-4 col-md-3">
-                <h5 class="card-title text-center fs-6">$ Non-Tunai</h5>
-                <div class="card info-card sales-card pt-4 card-dashboard">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="p-0">
-                                <h6 class="fs-5">Rp. {{ number_format($income['noncash'], 2) }} -,</h6>
-                                <span class="text-success small pt-1 fw-bold"></span> <span
-                                    class="text-muted small pt-2 ps-1">21 Februari 2023</span>
+            </div>
+            <div class="col-12">
+                <p class="fw-bold">Informasi keuangan retribusi tambahan:</p>
+                <div class="row">
+                    <!-- Revenue Card -->
+                    <div class="col-xxl-3 col-md-5">
+                        <a href="{{ route('dashboard.income') }}">
+                            <div class="card info-card revenue-card" data-bs-toggle="tooltip" data-bs-placement="right"
+                                data-bs-custom-class="custom-tooltip"
+                                data-bs-title="Klik untuk melihat lebih detail pemasukan">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pemasukan <span>| Bulan Ini</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-currency-dollar"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6 class="fs-4">Rp.
+                                                {{ number_format($income['cash'] + $income['noncash'], 2) }} -,</h6>
+                                            <span class="text-success small pt-1 fw-bold"></span><span
+                                                class="text-muted small pt-2 ps-1">21 Februari 2023</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div><!-- End Revenue Card -->
+                </div>
+            </div>
+            <div class="col-12">
+                <p class="fw-bold">Total uang yang terkumpul di bulan ini:</p>
+                <div class="row">
+                    <!-- Revenue Card -->
+                    <div class="col-xxl-3 col-md-4">
+                        <a href="{{ route('dashboard.income') }}">
+                            <div class="card info-card revenue-card" data-bs-toggle="tooltip" data-bs-placement="right"
+                                data-bs-custom-class="custom-tooltip"
+                                data-bs-title="Klik untuk melihat lebih detail pemasukan">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pemasukan <span>| Bulan Ini</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-currency-dollar"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6 class="fs-4">Rp.
+                                                {{ number_format($income['cash'] + $income['noncash'], 2) }} -,</h6>
+                                            <span class="text-success small pt-1 fw-bold"></span><span
+                                                class="text-muted small pt-2 ps-1">21 Februari 2023</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div><!-- End Revenue Card -->
+
+                    <!-- Sales Card -->
+                    <div class="col-xxl-4 col-md-5">
+                        <h5 class="card-title text-center fs-6">$ Tunai <span
+                                class="text-success small pt-1 fw-bold"></span> <span class="text-muted small pt-2">21
+                                Februari 2023</span>
+                        </h5>
+                        <div class="d-flex">
+                            <div class="card info-card sales-card pt-4 card-dashboard col-md-6">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-0">
+                                            <span class="text-success small pt-1 fw-bold"></span> <span
+                                                class="text-muted small pt-2">Sudah Disetor</span>
+                                            <h6 class="fs-5">Rp. {{ number_format($income['cash'], 2) }} -,</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card info-card sales-card pt-4 card-dashboard col-md-6" style="background-color: #55a63088">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-0">
+                                            <span class="text-success small pt-1 fw-bold"></span> <span
+                                                class="text-muted small pt-2">Belum Disetor</span>
+                                            <h6 class="fs-5">Rp. {{ number_format($income['cash'], 2) }} -,</h6>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- End Sales Card -->
+                    <div class="col-xxl-4 col-md-3">
+                        <h5 class="card-title text-center fs-6">$ Non-Tunai</h5>
+                        <div class="card info-card sales-card pt-4 card-dashboard">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="p-0">
+                                        <h6 class="fs-5">Rp. {{ number_format($income['noncash'], 2) }} -,</h6>
+                                        <span class="text-success small pt-1 fw-bold"></span> <span
+                                            class="text-muted small pt-2 ps-1">21 Februari 2023</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End Sales Card -->
                 </div>
-            </div><!-- End Sales Card -->
+            </div>
         </div>
     </div><!-- End Left side columns -->
 
