132d88b54d4e9cf621afa991bb2933bf80625255
andreaszico <zicoaritonang@gmail.com>
2023-05-07 16:42:38 +0700

    test

--- app/Http/Controllers/API/DokuController.php
+++ app/Http/Controllers/API/DokuController.php
@@ -10,25 +10,26 @@ class DokuController extends Controller
     public function notifications(Request $request)
     {
         try {
-            $notificationHeader = $request->header();
-            $notificationBody = $request->getContent();
+            $notificationHeader = getallheaders();
+            $notificationBody = file_get_contents('php://input');
             $notificationPath = '/payments/notifications'; // Adjust according to your notification path
-            $secretKey = env("DOKU_SECRET_KEY"); // Adjust according to your secret key
+            $secretKey = 'SK-e8acCt3iB1a1A0Jodfad'; // Adjust according to your secret key
 
             $digest = base64_encode(hash('sha256', $notificationBody, true));
-            $rawSignature = "Client-Id:" . $notificationHeader['Client-Id'][0] . "\n"
-                . "Request-Id:" . $notificationHeader['Request-Id'][0] . "\n"
-                . "Request-Timestamp:" . $notificationHeader['Request-Timestamp'][0] . "\n"
+            $rawSignature = "Client-Id:" . $notificationHeader['Client-Id'] . "\n"
+                . "Request-Id:" . $notificationHeader['Request-Id'] . "\n"
+                . "Request-Timestamp:" . $notificationHeader['Request-Timestamp'] . "\n"
                 . "Request-Target:" . $notificationPath . "\n"
                 . "Digest:" . $digest;
 
             $signature = base64_encode(hash_hmac('sha256', $rawSignature, $secretKey, true));
             $finalSignature = 'HMACSHA256=' . $signature;
 
-            if ($finalSignature === $notificationHeader['Signature'][0]) {
+            if ($finalSignature == $notificationHeader['Signature']) {
                 // TODO: Process if Signature is Valid
-                // You may want to parse the $notificationBody to extract the transaction details and update the transaction status.
                 return response('OK', 200)->header('Content-Type', 'text/plain');
+
+                // TODO: Do update the transaction status based on the `transaction.status`
             } else {
                 // TODO: Response with 400 errors for Invalid Signature
                 return response('Invalid Signature', 400)->header('Content-Type', 'text/plain');
